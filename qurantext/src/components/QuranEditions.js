a0ee191cd598d8e4e456896f4853dc51e4be7070
ibtisam34 <riyanagwl@gmail.com>
2023-07-05 15:48:06 +0300

    fix issues

--- qurantext/src/components/QuranEditions.js
+++ qurantext/src/components/QuranEditions.js
@@ -12,7 +12,8 @@ const QuranEditions = () => {
   const [selectedVerse, setSelectedVerse] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [selectedChapter, setSelectedChapter] = useState(null); // State variable to track the selected chapter
+  const [selectedChapter, setSelectedChapter] = useState(null);
+  const [bookmarks, setBookmarks] = useState([]); // Define the bookmarks state variable
   useEffect(() => {
     const fetchQuranicEditions = async () => {
       try {
@@ -27,7 +28,17 @@ const QuranEditions = () => {
   const handleChapterSelect = (chapter) => {
     setSelectedChapter(chapter);
   };
-  // Function to get the verses of the selected chapter
+  const handleSearch = (query) => {
+    // Implement the search functionality
+    // Update the searchResults state with the search results
+  };
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    // Implement logic to fetch and display verses of the selected page
+  };
+  const handleTranslationChange = (translation) => {
+    // Implement logic to handle translation change
+  };
   const getVersesOfChapter = (chapter) => {
     if (!chapter || !editions.length) return [];
     const chapterId = chapter.chapter_id;
@@ -35,32 +46,29 @@ const QuranEditions = () => {
     if (!edition) return [];
     return edition.verses;
   };
-  // Get the verses of the selected chapter
   const versesOfChapter = getVersesOfChapter(selectedChapter);
   return (
     <div>
       <h1>Quran Editions</h1>
-      {/* Display the selected chapter */}
       {selectedChapter && (
         <div>
           <h2>{selectedChapter.name_simple}</h2>
-          {/* Display the verses of the selected chapter */}
           {versesOfChapter.map((verse) => (
             <p key={verse.verse_key}>{verse.text_madani}</p>
           ))}
         </div>
       )}
       <SearchBar onSearch={handleSearch} />
-      <SearchResults results={searchResults} />
+      {/* Implement the SearchResults component */}
       <Pagination currentPage={currentPage} onPageChange={handlePageChange} />
-      <BookmarkList bookmarks={bookmarks} />
-      <RecitationPlayer reciter={selectedReciter} />
+      <BookmarkList bookmarks={bookmarks} setBookmarks={setBookmarks} />
+      {/* Implement the RecitationPlayer component */}
       <TranslationSwitcher
         translations={translations}
         selectedTranslation={selectedTranslation}
         onTranslationChange={handleTranslationChange}
       />
-      <TafsirDisplay verse={selectedVerse} />
+      {/* Implement the TafsirDisplay component */}
     </div>
   );
 };
