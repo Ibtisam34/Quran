b63e9a58a8a5967981e2d8b54abaafb4ea380e83
ibtisam34 <riyanagwl@gmail.com>
2023-07-10 13:24:54 +0300

    Remove reducers

--- qurantext/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import quranEditionsReducer from './quranEditionsReducer';
-const rootReducer = combineReducers({
-  quranEditions: quranEditionsReducer,
-});
-export default rootReducer;
