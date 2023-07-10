c27522a1d8f03a0d4fcadf1c9d67cc971ad83c44
ibtisam34 <riyanagwl@gmail.com>
2023-07-10 13:42:44 +0300

    Update quran

--- qurantext/src/components/Quran.js
+++ qurantext/src/components/Quran.js
@@ -1,33 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setQuranData } from '../redux/actions/quranActions';
 import axios from 'axios';
 const Quran = () => {
-  const [quranData, setQuranData] = useState(null);
+  const dispatch = useDispatch();
+  const quranData = useSelector(state => state);
   useEffect(() => {
     const fetchQuranData = async () => {
       try {
         const response = await axios.get('http://api.alquran.cloud/v1/quran');
-        setQuranData(response.data.data);
+        dispatch(setQuranData(response.data.data));
       } catch (error) {
         console.error('Error fetching Quran data:', error);
       }
     };
     fetchQuranData();
-  }, []);
+  }, [dispatch]);
   if (!quranData) {
     return <div>Loading...</div>;
   }
-  const renderAyahs = () => {
-    return quranData.surahs.map(surah => (
-      <div key={surah.number}>
-        <h2>{surah.englishName}</h2>
-        {surah.ayahs.map(ayah => (
-          <div key={ayah.number}>
-            <p>{ayah.text}</p>
-          </div>
-        ))}
-      </div>
-    ));
-  };
-  return <div>{renderAyahs()}</div>;
+  // Render the Quran data here
+  return <div>{/* Render the Quranic verses using the quranData */}</div>;
 };
 export default Quran;
