93016cd3c6cdd2d8e139406d1a867ff5888fc9e0
ibtisam34 <riyanagwl@gmail.com>
2023-07-10 13:35:42 +0300

    Render Quran data

--- qurantext/src/components/Quran.js
+++ qurantext/src/components/Quran.js
@@ -1,75 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import QuranApi from '../services/QuranApi';
-import VerseDisplay from './VerseDisplay';
-import Pagination from './Pagination';
-import SearchBar from './SearchBar';
-import BookmarkList from './BookmarkList';
-import RecitationPlayer from './RecitationPlayer';
-import TranslationSwitcher from './TranslationSwitcher';
-import TafsirDisplay from './TafsirDisplay';
-const QuranEditions = () => {
-  const [editions, setEditions] = useState([]);
-  const [selectedVerse, setSelectedVerse] = useState(null);
-  const [searchResults, setSearchResults] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [selectedChapter, setSelectedChapter] = useState(null);
-  const [bookmarks, setBookmarks] = useState([]); // Define the bookmarks state variable
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const Quran = () => {
+  const [quranData, setQuranData] = useState(null);
   useEffect(() => {
-    const fetchQuranicEditions = async () => {
+    const fetchQuranData = async () => {
       try {
-        const data = await QuranApi.fetchQuranicText();
-        setEditions(data);
+        const response = await axios.get('http://api.alquran.cloud/v1/quran');
+        setQuranData(response.data.data);
       } catch (error) {
-        console.log('Error fetching Quran editions:', error);
+        console.error('Error fetching Quran data:', error);
       }
     };
-    fetchQuranicEditions();
+    fetchQuranData();
   }, []);
-  const handleChapterSelect = (chapter) => {
-    setSelectedChapter(chapter);
+  if (!quranData) {
+    return <div>Loading...</div>;
+  }
+  const renderAyahs = () => {
+    return quranData.surahs.map(surah => (
+      <div key={surah.number}>
+        <h2>{surah.englishName}</h2>
+        {surah.ayahs.map(ayah => (
+          <div key={ayah.number}>
+            <p>{ayah.text}</p>
+          </div>
+        ))}
+      </div>
+    ));
   };
-  const handleSearch = (query) => {
-    // Implement the search functionality
-    // Update the searchResults state with the search results
-  };
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-    // Implement logic to fetch and display verses of the selected page
-  };
-  const handleTranslationChange = (translation) => {
-    // Implement logic to handle translation change
-  };
-  const getVersesOfChapter = (chapter) => {
-    if (!chapter || !editions.length) return [];
-    const chapterId = chapter.chapter_id;
-    const edition = editions.find((edition) => edition.chapter_id === chapterId);
-    if (!edition) return [];
-    return edition.verses;
-  };
-  const versesOfChapter = getVersesOfChapter(selectedChapter);
-  return (
-    <div>
-      <h1>Quran Editions</h1>
-      {selectedChapter && (
-        <div>
-          <h2>{selectedChapter.name_simple}</h2>
-          {versesOfChapter.map((verse) => (
-            <p key={verse.verse_key}>{verse.text_madani}</p>
-          ))}
-        </div>
-      )}
-      <SearchBar onSearch={handleSearch} />
-      {/* Implement the SearchResults component */}
-      <Pagination currentPage={currentPage} onPageChange={handlePageChange} />
-      <BookmarkList bookmarks={bookmarks} setBookmarks={setBookmarks} />
-      {/* Implement the RecitationPlayer component */}
-      <TranslationSwitcher
-        translations={translations}
-        selectedTranslation={selectedTranslation}
-        onTranslationChange={handleTranslationChange}
-      />
-      {/* Implement the TafsirDisplay component */}
-    </div>
-  );
+  return <div>{renderAyahs()}</div>;
 };
-export default QuranEditions;
+export default Quran;
